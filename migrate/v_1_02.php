eeda1a594d7284428a62b971de067712f9628ba3
jbhamilton <brad@bradleyhamilton.com>
2016-05-23 16:51:28 -0400

    updates for 1.03

---
+++ migrate/v_1_02.php
@@ -0,0 +1,6 @@
+<?php
+class TestPlugin_Migrate_v_1_02 {
+    public function migrate(){
+        \Enact::logError('Migration to version 1.02 success!');
+    }//migrate
+}
---
+++ migrate/v_1_03.php
@@ -0,0 +1,6 @@
+<?php
+class TestPlugin_Migrate_v_1_03 {
+    public function migrate(){
+        \Enact::logError('Migration to version 1.03 success!');
+    }//migrate
+}
